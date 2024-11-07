21bccc45731811ff362f406dab941a83e1b1eab3
iamfahad948 <fahadullah948@gmail.com>
2024-11-07 11:55:32 +0530

    .

--- SD-Hub-Portal/src/app/courses/courses.component.ts
+++ SD-Hub-Portal/src/app/courses/courses.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.css'
+  styleUrls: ['./courses.component.css'] // Corrected from styleUrl to styleUrls
 })
 export class CoursesComponent {
 
+  constructor(private router: Router) {}
+
+  redirectToRegister(): void {
+    // Navigate to the register page
+    this.router.navigate(['/register']);
+  }
 }
